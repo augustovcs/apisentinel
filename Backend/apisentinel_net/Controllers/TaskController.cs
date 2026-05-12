2df669af80694ca93aebc252969c4c6d30737a89
keven <mkminato123@gmail.com>
2026-05-12 11:09:41 -0300

    atualizar

--- Backend/apisentinel_net/Controllers/TaskController.cs
+++ Backend/apisentinel_net/Controllers/TaskController.cs
@@ -10,30 +10,5 @@ namespace Controllers.Development;
 
 public class FinanceController : ControllerBase
 {
-    public readonly FinancialOperations _FinancialOperations;
-
-    public FinanceController(FinancialOperations financialOperations)
-    {
-        _FinancialOperations = financialOperations;
-    }
-
-
-    [HttpGet("/Operation")]
-    
-    public async Task<ActionResult> search()
-    {
-        var algo = await _FinancialOperations.PostCalcularTudo(200, 20);
-
-        return Ok(algo);
-    }
-
-    [HttpPost("/Finance")]
-
-    public IActionResult PostFinance([FromBody] FinanceRequestDTO requestDto)
-    {
-        var SendPost = _FinancialOperations.Score(requestDto.ScoreDeCredito);
-
-        return Ok(SendPost);
-    }
     
 }
